ca7b6c36a669dd26f9def43b8f9b6ee060c3901c
inkkonito <pro.julienp@gmail.com>
2024-08-12 16:04:46 +0200

    restaurant table json booking

---
+++ 2_backend_strapi/week_2/restaurant/src/api/booking/controllers/booking.js
@@ -0,0 +1,63 @@
+// @ts-nocheck
+"use strict";
+
+const booking = require("../routes/booking");
+
+/**
+ * booking controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::booking.booking", ({ strapi }) => ({
+  async create(ctx) {
+    try {
+      const { seats, restaurant } = ctx.request.body.data; // seats and restaurant id requested are stored in variable
+
+      const getRestaurant = await strapi.entityService.findOne(
+        "api::restaurant.restaurant",
+        restaurant
+      );
+      const getTables = getRestaurant.tables; // storing tables available in variable
+
+      const findingTable = getTables.find((table) => table.capacity >= seats);
+
+      if (findingTable) {
+        console.log(
+          `Table found: ID ${findingTable.id} with capacity ${findingTable.capacity}`
+        );
+
+        const tableToUpdate = getTables.find(
+          (table) => table.id === findingTable.id
+        );
+
+        if (tableToUpdate) {
+          tableToUpdate.capacity -= seats;
+        }
+
+        await strapi.entityService.update(
+          "api::restaurant.restaurant",
+          restaurant,
+          {
+            data: {
+              tables: getTables,
+            },
+          }
+        );
+        const { meta, data } = await super.create(ctx);
+        data.attributes.tableSelected = findingTable.id;
+        data.attributes.restaurantRequested = restaurant;
+        return { meta, data };
+      } else {
+        return ctx.badRequest("No table found with enough capacity.", {
+          capacityRequested: seats,
+        });
+      }
+    } catch (error) {
+      return { message: error.message };
+    }
+  },
+  async delete(ctx) {
+    
+  }
+}));
