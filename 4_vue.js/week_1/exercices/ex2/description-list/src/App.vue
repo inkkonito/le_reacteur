ef2e0dee5180a16387cb5670c338e8c58ed975a2
inkkonito <pro.julienp@gmail.com>
2024-10-30 11:34:33 +0100

    adding bonus

--- 4_vue.js/week_1/exercices/ex2/description-list/src/App.vue
+++ 4_vue.js/week_1/exercices/ex2/description-list/src/App.vue
@@ -63,61 +63,86 @@ const fourthPerson = {
         <img src="./assets/img/emily-parker.jpg" alt="" />
         <div>
           <h2>{{ firstPerson.firstname }} {{ firstPerson.lastname }}</h2>
-          <p>She is {{ firstPerson.age }} years old {{ firstPerson.job }}.</p>
+          <p>
+            {{ firstPerson.gender === 'female' ? 'She' : 'He' }} is
+            {{ firstPerson.age }} years old {{ firstPerson.job.toLowerCase() }}.
+          </p>
           <p>
             Outside of work, {{ firstPerson.firstname }} likes to
             {{ firstPerson.hobbies[0] }} and {{ firstPerson.hobbies[1] }}.
           </p>
           <p>
-            At home her lively companion is {{ firstPerson.pet.name }} a
+            At home
+            {{ firstPerson.gender === 'female' ? 'her' : 'his' }} lively
+            companion is {{ firstPerson.pet.name }} a
             {{ firstPerson.pet.age }} years old {{ firstPerson.pet.species }}.
           </p>
         </div>
+        <font-awesome-icon :icon="['fas', 'heart']" />
       </section>
       <section>
         <img src="./assets/img/marcus-bennett.jpg" alt="" />
         <div>
           <h2>{{ secondPerson.firstname }} {{ secondPerson.lastname }}</h2>
-          <p>He is {{ secondPerson.age }} years old {{ secondPerson.job }}.</p>
+          <p>
+            {{ secondPerson.gender === 'female' ? 'She' : 'He' }} is
+            {{ secondPerson.age }} years old
+            {{ secondPerson.job.toLowerCase() }}.
+          </p>
           <p>
             Outside of work, {{ secondPerson.firstname }} likes to
             {{ secondPerson.hobbies[0] }} and {{ secondPerson.hobbies[1] }}.
           </p>
           <p>
-            At home his lively companion is {{ secondPerson.pet.name }} a
+            At home
+            {{ secondPerson.gender === 'female' ? 'her' : 'his' }} lively
+            companion is {{ secondPerson.pet.name }} a
             {{ secondPerson.pet.age }} years old {{ secondPerson.pet.species }}.
           </p>
         </div>
+        <font-awesome-icon :icon="['fas', 'heart']" />
       </section>
       <section>
         <img src="./assets/img/sophia-rodriguez.jpg" alt="" />
         <div>
           <h2>{{ thirdPerson.firstname }} {{ thirdPerson.lastname }}</h2>
-          <p>She is {{ thirdPerson.age }} years old {{ thirdPerson.job }}.</p>
+          <p>
+            {{ thirdPerson.gender === 'female' ? 'She' : 'He' }} is
+            {{ thirdPerson.age }} years old {{ thirdPerson.job.toLowerCase() }}.
+          </p>
           <p>
             Outside of work, {{ thirdPerson.firstname }} likes to
             {{ thirdPerson.hobbies[0] }} and {{ thirdPerson.hobbies[1] }}.
           </p>
           <p>
-            At home her lively companion is {{ thirdPerson.pet.name }} a
+            At home {{ thirdPerson.gender === 'female' ? 'her' : 'his' }} lively
+            companion is {{ thirdPerson.pet.name }} a
             {{ thirdPerson.pet.age }} years old {{ thirdPerson.pet.species }}.
           </p>
         </div>
+        <font-awesome-icon :icon="['fas', 'heart']" />
       </section>
       <section>
         <img src="./assets/img/olivier-hughes.jpg" alt="" />
         <div>
           <h2>{{ fourthPerson.firstname }} {{ fourthPerson.lastname }}</h2>
-          <p>He is {{ fourthPerson.age }} years old {{ fourthPerson.job }}.</p>
+          <p>
+            {{ fourthPerson.gender === 'female' ? 'She' : 'He' }} is
+            {{ fourthPerson.age }} years old
+            {{ fourthPerson.job.toLowerCase() }}.
+          </p>
           <p>
             Outside of work, {{ fourthPerson.firstname }} likes to
             {{ fourthPerson.hobbies[0] }} and {{ fourthPerson.hobbies[1] }}.
           </p>
           <p>
-            At home his lively companion is {{ fourthPerson.pet.name }} a
+            At home
+            {{ fourthPerson.gender === 'female' ? 'her' : 'his' }} lively
+            companion is {{ fourthPerson.pet.name }} a
             {{ fourthPerson.pet.age }} years old {{ fourthPerson.pet.species }}.
           </p>
         </div>
+        <font-awesome-icon :icon="['fas', 'heart']" />
       </section>
     </div>
   </main>
@@ -127,7 +152,7 @@ const fourthPerson = {
 main {
   padding: 50px;
   background-color: var(--green);
-  height: 100vh;
+  min-height: 100vh;
 }
 
 main > div {
@@ -143,6 +168,8 @@ section {
   border-radius: 10px;
   background-color: white;
   box-sizing: 0 0 7px grey;
+  display: flex;
+  flex-direction: column;
 }
 
 img {
@@ -156,5 +183,26 @@ img {
 p {
   line-height: 20px;
   margin-bottom: 5px;
+}
+
+svg {
+  color: grey;
+  align-self: flex-end;
+  margin-top: 20px;
+}
+
+/* ---- MEDIA QUERIES ---- */
+
+@media (max-width: 1250px) {
+  main {
+    /* background-color: lightblue; */
+  }
+  main > div {
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  section {
+    width: 280px;
+  }
 }
 </style>
