74d50b01008b0be6a0941427b17d3b207caccb52
inkkonito <pro.julienp@gmail.com>
2024-11-04 20:11:28 +0100

    fixing styles errors

---
+++ 4_vue.js/week_1/exercices/ex4/vue-style-error-main/src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+// Import du tableau contenant la liste des animaux
+import data from './assets/data.json'
+
+// Import du composant
+import Card from './components/Card.vue'
+</script>
+
+<template>
+  <main>
+    <h1>Animals for adoption</h1>
+
+    <div class="container">
+      <!-- Appel du composant 10 fois : une fois par animal -->
+      <Card :animalInfos="data[0]" />
+      <Card :animalInfos="data[1]" />
+      <Card :animalInfos="data[2]" />
+      <Card :animalInfos="data[3]" />
+      <Card :animalInfos="data[4]" />
+      <Card :animalInfos="data[5]" />
+      <Card :animalInfos="data[6]" />
+      <Card :animalInfos="data[7]" />
+      <Card :animalInfos="data[8]" />
+      <Card :animalInfos="data[9]" />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 50px;
+  background-color: var(--secondary-bgc);
+  min-height: 100vh;
+  min-width: 100vw;
+}
+main > div {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 15px;
+  flex: 1;
+  /* width: 65vw; */
+}
+
+/* @media (max-width: 900px) {
+  main {
+    background-color: pink;
+  }
+}
+@media (max-width: 500px) {
+  main {
+    background-color: blue;
+  }
+} */
+</style>
---
+++ 4_vue.js/week_1/exercices/ex4/vue-style-error-main/src/components/Card.vue
@@ -0,0 +1,61 @@
+<script setup>
+import Details from '../components/Details.vue'
+// Déclaration de la props qui contient les informations d'un animal
+const props = defineProps({
+  animalInfos: Object
+})
+</script>
+
+<template>
+  <div class="card" :class="{ opacity: animalInfos.alreadyAdopted }">
+    <Details :animalInfos="animalInfos" />
+
+    <p class="description">{{ animalInfos.description }}</p>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  background-color: var(--bgc);
+  padding: 15px;
+  border-radius: 5px;
+  width: calc((100% - 15px) / 2);
+  display: flex;
+  gap: 10px;
+  box-shadow: 0px 0px 7px var(--shadow-color);
+  display: flex;
+  flex-direction: row;
+}
+.card > div {
+  width: 150px;
+  display: flex;
+  flex-direction: column;
+  /* justify-content: space-between; */
+  gap: 10px;
+}
+.description {
+  flex: 1;
+}
+p span {
+  text-decoration: underline;
+  color: gray;
+}
+.card p {
+  color: var(--txt-color);
+}
+
+.opacity {
+  opacity: 0.3;
+}
+
+@media (max-width: 900px) {
+  .card {
+    width: 100%;
+  }
+}
+@media (max-width: 500px) {
+  .card {
+    flex-direction: column;
+  }
+}
+</style>
