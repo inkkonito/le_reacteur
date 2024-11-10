4d5cb0dfda190ba057b261bd0fb68e088278bbd5
inkkonito <pro.julienp@gmail.com>
2024-11-10 17:37:55 +0100

    reactive counter

---
+++ 4_vue.js/week_2/ex2/counter/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+const counter = ref(0)
+
+const decrement = () => {
+  counter.value--
+}
+const increment = () => {
+  counter.value++
+}
+const reset = () => {
+  counter.value = 0
+}
+</script>
+<template>
+  <h1>Counter</h1>
+  <div>
+    <button @click="decrement">-</button>
+    {{ counter }}
+    <button @click="increment">+</button>
+  </div>
+  <div>
+    <button @click="reset">Reset</button>
+  </div>
+</template>
+
+<style scoped></style>
